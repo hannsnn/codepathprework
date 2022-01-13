c15e9662bf31c839f533356382850023b4e543ef
hannsnn <97542264+hannsnn@users.noreply.github.com>
2022-01-12 22:11:53 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  pre work2
+//
+//  Created by 陈思晗 on 1/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // Get Total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill *
+        tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        //update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+}
